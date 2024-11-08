0319e3779085eedcc4f77e499f71b6f7a7278657
sameera093 <sameera.rath093@gmail.com>
2024-11-08 17:34:42 +0530

    this is my eighth commit

---
+++ demo8.py
@@ -0,0 +1,10 @@
+a=220
+b=330
+if b>a :
+    print("b is greater than a.")
+elif a==b :
+    print("a and b, both are equal.")
+elif a>b :
+    print("a is greater than b.")
+else :
+    print("all the above conditions are false")
